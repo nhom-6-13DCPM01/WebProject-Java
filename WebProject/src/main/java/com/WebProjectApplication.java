389fe6efd40ebc45e2cb94baa6a2d839975bf45b
PhanNhutTruong <tuong22223@gmail.com>
2022-04-14 23:47:56 +0700

    start login

--- WebProject/src/main/java/com/WebProjectApplication.java
+++ WebProject/src/main/java/com/WebProjectApplication.java
@@ -3,9 +3,8 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class WebProjectApplication {
 
 	public static void main(String[] args) {
