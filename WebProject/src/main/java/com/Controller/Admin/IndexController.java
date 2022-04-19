9ddde18df2ba7d194be222d65636b5af681b2d72
quynhgiang26 <nguyenthiquynhgiang261200@gmail.com>
2022-04-19 15:29:52 +0700

    commit

--- WebProject/src/main/java/com/Controller/Admin/IndexController.java
+++ WebProject/src/main/java/com/Controller/Admin/IndexController.java
@@ -1,10 +1,10 @@
 package com.Controller.Admin;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping("/Admin")
@@ -15,10 +15,7 @@ public class IndexController {
         
         return "Admin/index";
     }
-    @GetMapping("/Categories")
-    public String CategoriesShow(){
-        return "Admin/categories";
-    }
+    
     
 
 }
