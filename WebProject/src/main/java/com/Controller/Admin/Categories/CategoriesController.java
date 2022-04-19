8d787fddfa68b4bc0efd26d9a241560612acea49
quynhgiang26 <nguyenthiquynhgiang261200@gmail.com>
2022-04-19 15:27:42 +0700

    delete 2

--- WebProject/src/main/java/com/Controller/Admin/Categories/CategoriesController.java
+++ WebProject/src/main/java/com/Controller/Admin/Categories/CategoriesController.java
@@ -1,10 +1,10 @@
 package com.Controller.Admin.Categories;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/Admin/Categories")
